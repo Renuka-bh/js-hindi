1f541449c8cdd49e595f98d7638aad0a0a28a5c9
Renuka Pawar <132355618+Renuka-bh@users.noreply.github.com>
2024-02-19 07:14:08 +0000

    dataType-summary

---
+++ .devcontainer/01_basics/dataTypes-summary.js
@@ -0,0 +1,59 @@
+/*
+~~~ Primitive ~~~ -->call by value
+7 types
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+*/
+
+
+/*
+~~~~~ Referance type/ non-primitive data type  ~~~~~~~~
+Array
+Objects
+Functions
+*/
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let useremail 
+
+/* 
+JavaScript is a dynamically typed language, 
+which means that data types of variables are determined by 
+the value they hold at runtime and can change throughout the program 
+as we assign different values to them
+*/ 
+
+// Using Symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)   //false
+
+//using BigInt
+
+const bigNmber = 245596882311n
+
+console.log(typeof bigNmber)   //bigint
+
+//Arrays
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+let myObj = {
+    name: "Renuka",
+    age: 25
+}
+
+const myfunction = function() {
+console.log("Hello World");
+}
+
+console.log(typeof myfunction)

b889b93952783cbb58b4381335dd6dcbadd153b3
Renuka Pawar <132355618+Renuka-bh@users.noreply.github.com>
2024-02-19 05:39:57 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 121456
+let accountEmail = "renukapawar@gmail.com"
+var accountPassword = "Renuka12345"
+accountCity = "Dhule"   // variable can be declared directly also
+let accountState        // if a variable is declared but value is not assigned it's value is undefined
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/ 
+
+console.log(accountEmail);
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState] )
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+// earlier classes, modules were not there.. so there is difference between old and new JS, but we can still use old codes
+//but just to use new JS "use strict"
+
+"use strict"; // treat all JS code as newer version.. now a days no need to defione use strct it's automatically considered as newer
+
+// alert("hello")    // we are using node.js not browser
+
+let name = "Renuka"    // string
+let age = 25           //number
+let isLoggedin = false // boolean
+
+
+/*
+number    => 2 to power 53
+bigint
+string    => "" or ''
+boolean   => true or false
+null      => standalone value = empty value =>typeof(null)=>object
+undefined => value is not defined 
+symbol    =>unique --> to find uniqueness-- most used in react
+
+object
+
+*/
+
+console.log(typeof null)
